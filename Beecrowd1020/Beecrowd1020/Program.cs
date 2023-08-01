2813b5ade7c2133ca43748fd68eb7a444cd4faac
João Paulo Oliveira <80533903+zeroYJPBO@users.noreply.github.com>
2023-08-01 19:02:44 -0300

    Add files via upload

---
+++ Beecrowd1020/Beecrowd1020/Program.cs
@@ -0,0 +1,33 @@
+﻿// Vigésimo exercício resolvido por João Paulo Bezerra Oliveira
+// Curso: C# primeiros passos: Lógica de Programação e Algoritmos - Udemy
+// Curitiba, 1 de agosto de 2023
+
+using System;
+
+namespace Beecrowd1020
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 0;
+
+            idade = int.Parse(Console.ReadLine());
+
+            int calcAnos = 0;
+
+            calcAnos = idade / 365;
+
+            int calcMeses = 0;
+
+            calcMeses = (idade % 365) / 30;
+
+            int calcDias = (idade % 365) % 30;
+
+            Console.WriteLine(calcAnos + " ano(s)");
+            Console.WriteLine(calcMeses + " mes(es)");
+            Console.WriteLine(calcDias + " dia(s)");
+
+        }
+    }
+}
