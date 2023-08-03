88a08936c563080fc3b3d5226d6c2f41e432f8d0
João Paulo Oliveira <80533903+zeroYJPBO@users.noreply.github.com>
2023-08-03 18:53:45 -0300

    Add files via upload

---
+++ Beecrowd1159/Beecrowd1159/Program.cs
@@ -0,0 +1,35 @@
+﻿// Trigésimo segundo exercício resolvido por João Paulo Bezerra Oliveira
+// Curso: C# primeiros passos: Lógica de Programação e Algoritmos - Udemy
+// Curitiba, 3 de agosto de 2023
+
+using System;
+
+namespace Beecrowd1159
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int X = 0;
+
+            X = int.Parse(Console.ReadLine());
+
+            while (X != 0)
+            {
+
+                if (X % 2 == 0)
+                {
+                    X = X + (X + 2) + (X + 4) + (X + 6) + (X + 8);
+                    Console.WriteLine(X);
+                }
+                else
+                {
+                    X = (X + 1) + (X + 3) + (X + 5) + (X + 7) + (X + 9);
+                    Console.WriteLine(X);
+                }
+
+                X = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
